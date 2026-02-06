3e8ef6753a7bcb5196db4bcffeda5052e5607fa3
codeislive63 <codeislive63@gmail.com>
2026-02-06 11:18:08 +0300

    feat(templates): add embedded default end label HTML template

--- src/LabelFlowStudio.Templates/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LabelFlowStudio.Templates
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/LabelFlowStudio.Templates/TemplateDefaults.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+
+namespace LabelFlowStudio.Templates;
+
+public static class TemplateDefaults
+{
+    public static string GetEndLabelHtml()
+    {
+        return ReadEmbeddedTextEndingWith("EndLabel.EndLabel.html");
+    }
+
+    private static string ReadEmbeddedTextEndingWith(string resourceNameEnding)
+    {
+        var assembly = Assembly.GetExecutingAssembly();
+
+        var resourceName = assembly
+            .GetManifestResourceNames()
+            .SingleOrDefault(name => name.EndsWith(resourceNameEnding, StringComparison.Ordinal));
+
+        if (resourceName is null)
+        {
+            throw new InvalidOperationException($"Template resource not found: *{resourceNameEnding}");
+        }
+
+        using var stream = assembly.GetManifestResourceStream(resourceName);
+        if (stream is null)
+        {
+            throw new InvalidOperationException($"Template resource stream not found: {resourceName}");
+        }
+
+        using var reader = new StreamReader(stream);
+        return reader.ReadToEnd();
+    }
+}
